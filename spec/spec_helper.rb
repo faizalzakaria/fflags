eb30c3e3e53e0b11bfedce506a86f62f60efc784
Faizal Zakaria <fai@code3.io>
2017-12-27 13:55:15 +0100

    Added test coverage (simplecov)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,13 @@
+require 'simplecov'
+
+# save to CircleCI's artifacts directory if we're on CircleCI
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
+SimpleCov.start
+
 require 'bundler/setup'
 require 'fflags'
 
